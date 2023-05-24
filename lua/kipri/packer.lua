56fe1127d49c2e3a2f078e2a6bfb633085ae4a9c
KIPRI <hi@kipri.dev>
2023-05-25 00:31:10 +0300

    cleaned packer.luawv

--- lua/kipri/packer.lua
+++ lua/kipri/packer.lua
@@ -1,15 +1,6 @@
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-local is_bootstrap = false
-
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    is_bootstrap = true
-    vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
-    vim.cmd [[packadd packer.nvim]]
-end
-
 require('packer').startup(function(use)
     -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+    use { 'wbthomason/packer.nvim' }
 
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
@@ -21,8 +12,11 @@ require('packer').startup(function(use)
         }
     }
 
-    use { 'nvim-telescope/telescope-fzf-native.nvim', run =
-    'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+    use {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        run =
+        'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+    }
 
     use {
         'olivercederborg/poimandres.nvim',
@@ -67,11 +61,14 @@ require('packer').startup(function(use)
         end
     }
 
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-    use('nvim-treesitter/playground')
-    use('theprimeagen/harpoon')
-    use('mbbill/undotree')
-    use('tpope/vim-fugitive')
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate',
+    }
+    use { 'nvim-treesitter/playground' }
+    use { 'theprimeagen/harpoon' }
+    use { 'mbbill/undotree' }
+    use { 'tpope/vim-fugitive' }
     use {
         'preservim/nerdtree',
         config = function()
@@ -93,30 +90,7 @@ require('packer').startup(function(use)
     }
 
     use { 'feline-nvim/feline.nvim' }
-
-    -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
-    local has_plugins, plugins = pcall(require, 'custom.plugins')
-    if has_plugins then
-        plugins(use)
-    end
-
-    if is_bootstrap then
-        require('packer').sync()
-    end
 end)
-
--- When we are bootstrapping a configuration, it doesn't
--- make sense to execute the rest of the init.lua.
---
--- You'll need to restart nvim, and then it will work.
-if is_bootstrap then
-    print '=================================='
-    print '    Plugins are being installed'
-    print '    Wait until Packer completes,'
-    print '       then restart nvim'
-    print '=================================='
-    return
-end
 
 -- Automatically source and re-compile packer whenever you save this init.lua
 local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
